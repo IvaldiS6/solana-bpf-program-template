d1e27e88df8ebe537e6a4fa73a5ca7a745a34de9
IvaldiS6 <bran.jacobson@gmail.com>
2022-05-05 00:09:26 -0700

    Trying out the program...

--- src/instructions.rs
+++ src/instructions.rs
@@ -3,18 +3,6 @@ use solana_program::program_error::ProgramError;
 
 use crate::error::EscrowError::InvalidInstruction;
 
-pub enum EscrowError {
-    ///Invalid instruction
-    #[error("Invalid Instruction")]
-    InvalidInstruction,
-}
-
-impl From<EscrowError> for ProgramError {
-    fn from(e: EscrowError) -> Self {
-        ProgramError::Custom(e as u32)
-    }
-}
-
 pub enum EscrowInstruction {
 
     /// Accounts expected:
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,7 @@ pub mod entrypoint;
 pub mod instructions;
 pub mod error;
 pub mod processor;
+pub mod state;
+
+#[cfg(not(feature = "no-entrypoint"))]
+mod entrypoint;
