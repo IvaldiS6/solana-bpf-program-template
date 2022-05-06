a10921c3ca97d1e938380dd05b430ce14ce0edf3
IvaldiS6 <bran.jacobson@gmail.com>
2022-05-06 03:39:46 -0700

    copy from source

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-pub mod entrypoint;
+///pub mod entrypoint;
 pub mod instructions;
 pub mod error;
 pub mod processor;
 pub mod state;
 
 #[cfg(not(feature = "no-entrypoint"))]
-mod entrypoint;
+pub mod entrypoint;
